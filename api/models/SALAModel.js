99a871a291bd05a389398ef678bd5a29a50d181e
Kevin Leiva <Kevinomg9@gmail.com>
2022-04-25 10:52:54 -0400

    a

--- api/models/SALAModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var SALASchema = new Schema({
-  name: String,
-  seats: [],
-});
-
-module.exports = mongoose.model("SALA", SALASchema);
--- api/routes/product.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var ProductController = require("../controllers/ProductsController")
-
-router.get("/i", ProductController.all);
-
-
-
-module.exports = router;
